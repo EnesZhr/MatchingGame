cdfbfc4d69b779e137e356d645f6707625755d33
enes zehir <enes.zehir@hotmail.com>
2021-09-04 13:00:56 +0300

    Add project files.

---
+++ MatchingGame/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace MatchingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 549);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Matching Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ MatchingGame/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+        int satir = 4, sutun = 4;
+        int adet, cift, gen, yuk, pay = 10;
+        Random rnd = new Random();
+        int [] tumResimNolar;
+        int[] kartlar;
+
+        public Form1()
+        {
+            InitializeComponent();
+            OyunuBaslat();
+        }
+
+        void OyunuBaslat()
+        {
+            tumResimNolar = new int[263];
+            for (int i = 0; i < 263; i++)
+            {
+                tumResimNolar[i] = i;
+                Karistir(tumResimNolar);
+            }
+        }
+
+        private void Karistir(int[] dizi)
+        {
+            int yedek;
+            int indeks;
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                indeks = rnd.Next(i, dizi.Length);
+                yedek = dizi[i];
+                dizi[i] = dizi[indeks];
+                dizi[indeks] = yedek;
+            }
+        }
+    }
+}
