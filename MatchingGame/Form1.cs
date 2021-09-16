8cd9ae84aa006cc7933548b1cd1764b146a85dce
enes zehir <enes.zehir@hotmail.com>
2021-09-16 11:58:36 +0300

    Yeniden oyunu başlatma ve kartların kapatılması

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,10 +15,12 @@ namespace MatchingGame
     public partial class Form1 : Form
     {
         int satir = 4, sutun = 4;
-        int adet, cift, gen, yuk, pay = 10,bulunanCiftAdet;
+        int adet, cift, gen, yuk, pay = 10, bulunanCiftAdet;
         Random rnd = new Random();
         int [] tumResimNolar;
         int[] kartlar;
+        List<PictureBox> aciklar;
+        Button btnYenidenBaslat;
 
         public Form1()
         {
@@ -27,7 +30,22 @@ namespace MatchingGame
 
         void OyunuBaslat()
         {
+            pnlKartlar.Controls.Clear();
+
+            #region Yeniden Başlat Butonunun Eklenip Gizlenmesi
+            btnYenidenBaslat = new Button();
+            btnYenidenBaslat.Text = "YENİDEN BAŞLAT";
+            btnYenidenBaslat.Location = new Point(192, 233);
+            btnYenidenBaslat.Size = new Size(180, 35);
+            btnYenidenBaslat.Click += BtnYenidenBaslat_Click;
+            btnYenidenBaslat.Visible = false;
+            pnlKartlar.Controls.Add(btnYenidenBaslat);
+
+            #endregion
+
             #region Oyun Değişkenlerinin Hesaplanması
+            bulunanCiftAdet = 0;
+            aciklar = new List<PictureBox>();
             adet = satir * sutun;
             cift = adet / 2;
             gen = (pnlKartlar.Width - (sutun - 1) * pay) / sutun;
@@ -59,7 +77,6 @@ namespace MatchingGame
                 pb.SizeMode = PictureBoxSizeMode.Zoom;
                 pb.BackColor = Color.Gray;
                 pb.Tag = kartlar[i];
-                pb.Image = (Image)Resources.ResourceManager.GetObject("_" + kartlar[i]);
                 pb.Width = gen;
                 pb.Height = yuk;
                 pb.Click += KartaTiklandiginda;
@@ -69,10 +86,67 @@ namespace MatchingGame
 
         }
 
+        private void BtnYenidenBaslat_Click(object sender, EventArgs e)
+        {
+            OyunuBaslat();
+        }
+
         private void KartaTiklandiginda(object sender, EventArgs e)
         {
             PictureBox tiklanan = (PictureBox)sender;
+
+            if (aciklar.Contains(tiklanan))
+                return;
+
+            if (aciklar.Count == 2)
+                AcikKartlariKapat();
+
+            KartiAc(tiklanan);
+
+            if(aciklar.Count == 2 && (int)aciklar[0].Tag == (int)aciklar[1].Tag)
+            {
+                Refresh();
+                Thread.Sleep(400);
+                AciklariKapatGizle();
+                bulunanCiftAdet++;
+
+                if(bulunanCiftAdet == cift)
+                {
+                    MessageBox.Show("Oyun Bitti!!");
+                    btnYenidenBaslat.Show();
+                }
+            }
             
+        }
+        private void AciklariKapatGizle()
+        {
+            while (aciklar.Count > 0)
+            {
+                aciklar[0].Hide();
+                KartiKapat(aciklar[0]);
+            }
+        }
+
+        void AcikKartlariKapat()
+        {
+            while (aciklar.Count > 0)
+            {
+                KartiKapat(aciklar[0]);
+            }
+        }
+
+        void KartiAc(PictureBox kart)
+        {
+            int resimNo = (int)kart.Tag;
+            kart.Image = (Image)Resources.ResourceManager.GetObject("_" + resimNo);
+            aciklar.Add(kart);
+        }
+
+        void KartiKapat(PictureBox kart)
+        {
+            int resimNo = (int)kart.Tag;
+            kart.Image = null;
+            aciklar.Remove(kart);
         }
 
         private void Karistir(int[] dizi)
